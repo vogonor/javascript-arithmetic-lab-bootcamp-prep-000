664a270883dcd9ea64e12daa332b453a9caa0aa1
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-14 13:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+function add(a, b) {
+  return a+b
+}
+
+
+function subtract(a, b) {
+  return a-b
+}
+
+function multiply (a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+  
+}
+
+function increment(n) {
+  
+  return n + 1
+}
+
+function decrement (n) {
+  return n-1
+}
+
+function makeInt(n) {
+  
+  if(isNaN(n))
+     return 0;
+   return parseInt(n)
+  
+}
+
+
+function preserveDecimal(string){
+  return parseFloat(string)
+}
